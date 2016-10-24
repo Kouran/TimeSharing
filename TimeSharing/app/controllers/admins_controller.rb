50c941326c14ba1622ba10ee5bf37fb38b0a45f2
Pasquale Scognamiglio <thegameita@gmail.com>
2016-10-24 18:33:00 +0200

    Ciao

---
+++ TimeSharing/app/controllers/admins_controller.rb
@@ -0,0 +1,7 @@
+class AdminsController < ApplicationController
+	def admin
+	end
+
+	def mod
+	end
+end
--- TimeSharing/config/routes.rb
+++ TimeSharing/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+get 'admins/admin' => 'admins#admin'
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
