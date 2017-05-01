0659970a4aee3d69a5e035d84110c1929c90b6bb
Pasquale Scognamiglio <kouran@debian>
2017-05-01 23:00:17 +0200

    Riorganizzazione struttura cartelle

--- timesharing.sh
+++
@@ -1,4 +0,0 @@
-#Script per l'avvio dell'applicazione TimeSharing
-rake db:setup
-curl -F "url=https://timesharing.pagekite.me/webhook" https://api.telegram.org/bot371175469:AAFZjsjbupLsvM4o7I0VPAADRd5cM_6-Y70/setWebhook
-rails s -p 8443
